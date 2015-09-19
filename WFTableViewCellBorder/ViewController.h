da3c3aad8a24c315b3877a622f5106a0a5c1ec2a
wufei1423 <wufei1423@163.com>
2015-09-19 10:11:40 +0800

    init

---
+++ WFTableViewCellBorder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WFTableViewCellBorder
+//
+//  Created by feiwu on 15/9/19.
+//  Copyright (c) 2015年 feiwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WFTableViewCellBorder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WFTableViewCellBorder
+//
+//  Created by feiwu on 15/9/19.
+//  Copyright (c) 2015年 feiwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
